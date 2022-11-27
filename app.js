08f69d4e42400c2cdbb4e4ccf4ed8d62ce0e1ca9
Valeriya Druchinina <vchumack@glmedia.org>
2022-11-27 21:10:30 +0200

    fix

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-
-require("dotenv").config();
 const contactsRouter = require("./routes/api/contacts");
+require("dotenv").config();
 
 const app = express();
 
